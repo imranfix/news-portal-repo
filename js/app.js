4a54604aee8b1c191beb8f264edc5c203426a944
imranfix <imran2000hosan@gmail.com>
2022-09-03 16:17:32 +0600

    added card modal section

--- js/app.js
+++ js/app.js
@@ -44,7 +44,8 @@ const displayMenu = datas =>{
         cardDiv.innerHTML= `
         <div class="card mb-3 d-flex" >
         <div class="col-md-4 col-sm-6">
-        <img src="${data.image_url}" class="image-radious mt-2 img-fluid rounded-start p-3" alt="...">
+        <img src="${data.thumbnail_url
+        }" class="image-radious mt-2 img-fluid rounded-start p-3" alt="...">
       </div>
         <div class="col-md-8 col-sm-12">
             <div class="card-body">
@@ -55,12 +56,11 @@ const displayMenu = datas =>{
         <div class="d-flex mt-2 mb-2">  
             <img src="${data.author.img}" class="test rounded-circle" alt="">
             <h6>${data.author.name}</h6>
-            <h5 class="px-5 text-secondary"> <i class="fa-regular fa-eye"></i> 1.5M</h5>
-           
+            <h5 class="px-5 text-secondary"> <i class="fa-regular fa-eye"></i>${data.total_view}</h5>
 
+            <div><button onclick="loadNewsDetails()" type="button" class="btn btn-info fw-semibold text-white ms-4"  data-bs-toggle="modal" data-bs-target="#newsModal">Details</button></div>
             
-            <h5 class=""><i class="fa-solid fa-right-long text-primary text-xl-end mt-2 ps-4"></i></h5>
-        
+
             
         </div>
            
@@ -73,13 +73,34 @@ const displayMenu = datas =>{
         });
         
        } 
-       loadCards();
 
 
 
 
+// card modal -
+const loadNewsDetails = async() =>{
+    const url = 'https://openapi.programming-hero.com/api/news/0282e0e58a5c404fbd15261f11c2ab6a';
+    const response = await fetch(url);
+    const data = await response.json();
+     displayNewsDetails(data.data[0]);
+   
+}
+
+    const displayNewsDetails = data =>{
+        console.log(data);
+        const modalTitle = document.getElementById('newsModalLabel');
+        modalTitle.innerText = data.title;
+        const newsDetails = document.getElementById('news-details');
+        newsDetails.innerText = data.category_id;
+        const newsView = document.getElementById('news-view');
+        newsView.innerText = data.total_view;
+        const detailsModal = document.getElementById('details-modal');
+        detailsModal.innerText = data.details.slice(0, 180);
+    }
 
 
-    
-    
-     loadMenuBar();
+
+
+    loadCards();
+
+    loadMenuBar();
